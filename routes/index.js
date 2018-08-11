ed8cb0cb80e708e175b91ae310efb27879b35a5d
stevegordon <tdtsgordon@gmail.com>
2018-08-11 12:14:51 -0700

    routes view files jade block content

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
+});
+
+router.get('/slimes', function(req, res, next) {
+  res.render('slimes', { title: 'Slimes' });
+});
+
+router.get('/guides', function(req, res, next) {
+  res.render('guides', { title: 'Guides'});
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact'});
 });
 
 module.exports = router;
