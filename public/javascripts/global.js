a917ab9426ff248176bba8c80f02374663d9c0b7
stevegordon <tdtsgordon@gmail.com>
2018-08-14 23:55:19 -0700

    css animations

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -2,9 +2,14 @@
 
 document.addEventListener('scroll', function (e) {
   var top = window.pageYOffset + window.innerHeight,
-  isVisible = top > document.querySelector('#test > div').offsetTop;
-
-  if (isVisible) {
-    document.getElementById('test').classList.add('animate');
+  // isVisible = top > document.querySelector('#test > div').offsetTop;
+  isVisible0 = top > document.querySelector('.card-deck > div').offsetTop,
+  isVisible1 = top > document.querySelector('.portfolio > div').offsetTop;
+  if (isVisible0) {
+    document.getElementsByClassName('card-deck')[0].classList.add('animate');
+  }
+  if (isVisible1) {
+    document.getElementsByClassName('portfolio')[0].classList.add('animate');
   }
 });
+
